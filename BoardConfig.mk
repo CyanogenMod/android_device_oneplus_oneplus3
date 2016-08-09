221a9e912624c1dc6bb5fcb0a2b56cfe463bb80b
Kristof Petho <kristof.petho@gmail.com>
2016-08-09 13:25:12 +0200

    Disable dexpreopt
    Change-Id: I8fd146acd60aae84116eb6849ffd7d23da4b290f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,13 +145,13 @@ MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 # Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
+# ifeq ($(HOST_OS),linux)
+#   ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+#     ifeq ($(WITH_DEXPREOPT),)
+#       WITH_DEXPREOPT := true
+#     endif
+#   endif
+# endif
 
 # GPS
 TARGET_NO_RPC := true
